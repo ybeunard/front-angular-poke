e48ec4a2312a784aba0f076916d902e4c7970fd7
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-05-31 15:04:09 +0200

    Addition of e2e tests and associated configuration

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FrontOpsPage } from './app.po';
+
+describe('front-ops App', () => {
+  let page: FrontOpsPage;
+
+  beforeEach(() => {
+    page = new FrontOpsPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
