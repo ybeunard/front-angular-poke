b0e67aa98f94c8a08c511ab2a2b4355170b387e5
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-06-07 14:42:24 +0200

    [FEAT] - Add toolbar component

---
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -0,0 +1,39 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MdIconModule, MdToolbarModule } from '@angular/material';
+
+import { ToolbarComponent } from './toolbar.component';
+
+describe('ToolbarComponent', () => {
+
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [ ToolbarComponent ],
+      imports: [
+        MdIconModule,
+        MdToolbarModule
+      ]
+    })
+    .compileComponents();
+
+  }));
+
+  beforeEach(() => {
+
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+  });
+
+  it('should be created', () => {
+
+    expect(component).toBeTruthy();
+
+  });
+
+});
