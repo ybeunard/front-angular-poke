1ece8b17a87aca49a2633d0d08cbb057ad0720ec
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-06-07 14:45:40 +0200

    [FEAT] - Update app module and component to use new components: header and toolbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 
+import { MdIconModule, MdToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
 
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ToolbarComponent,
+        HeaderComponent
       ],
+      imports: [
+        MdIconModule,
+        MdToolbarModule
+      ]
     }).compileComponents();
+
   }));
 
   it('should create the app', async(() => {
+
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MdIconModule, MdToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdIconModule,
+    MdToolbarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  bootstrap: [ AppComponent ]
+
+}) export class AppModule { }
