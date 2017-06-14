4b393943329cd8840b661f58252ebf49c2276f4f
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-06-14 10:38:06 +0200

    [REFRACTO] - Upgrade of i18n translation

--- src/locale/messages.en.ts
+++ src/locale/messages.en.ts
@@ -8,7 +8,7 @@ export const TRANSLATE_EN = `
         <target>Hello i18n!</target>
         <context-group purpose="location">
           <context context-type="sourcefile">C:/Users/ybeunard/workspace/front-ops/src/app/toolbar/toolbar.component.ts</context>
-          <context context-type="linenumber">2</context>
+          <context context-type="linenumber">5</context>
         </context-group>
       </trans-unit>
       <trans-unit id="030b4423b92167200e39519599f9b863b4f7c62c" datatype="html">
@@ -16,9 +16,33 @@ export const TRANSLATE_EN = `
         <target>Actions</target>
         <context-group purpose="location">
           <context context-type="sourcefile">C:/Users/ybeunard/workspace/front-ops/src/app/toolbar/toolbar.component.ts</context>
+          <context context-type="linenumber">8</context>
+        </context-group>
+      </trans-unit>
+      <trans-unit id="1a0050c239e6908de1bde5f34ca3ac38a375e502" datatype="html">
+        <source>Front ops</source>
+        <target>Project Front ops</target>
+        <context-group purpose="location">
+          <context context-type="sourcefile">C:/Users/ybeunard/workspace/front-ops/src/app/header/header.component.ts</context>
           <context context-type="linenumber">5</context>
+        </context-group>
+      </trans-unit>
+      <trans-unit id="a0843de0a64adcc7ee6a946408b6876cd8abdcae" datatype="html">
+        <source>English</source>
+        <target>English</target>
+        <context-group purpose="location">
+          <context context-type="sourcefile">C:/Users/ybeunard/workspace/front-ops/src/app/header/header.component.ts</context>
+          <context context-type="linenumber">8</context>
+        </context-group>
+      </trans-unit>
+      <trans-unit id="8293a584b57b870aca184a23bb8039c829acc4cf" datatype="html">
+        <source>French</source>
+        <target>French</target>
+        <context-group purpose="location">
+          <context context-type="sourcefile">C:/Users/ybeunard/workspace/front-ops/src/app/header/header.component.ts</context>
+          <context context-type="linenumber">9</context>
         </context-group>
       </trans-unit>
     </body>
   </file>
-</xliff>`
+</xliff>`;
