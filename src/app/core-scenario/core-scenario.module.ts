306f1c3211835c1a3a62e5e466afba790197ab70
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-06-23 13:27:27 +0200

    [FEAT] - add mapping routing in core-scenario to display scenario component

--- src/app/core-scenario/core-scenario.module.ts
+++ src/app/core-scenario/core-scenario.module.ts
@@ -1,17 +1,37 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import {MdButtonModule, MdMenuModule} from "@angular/material";
 
 import { CoreScenarioRoutingModule } from "./core-scenario-routing";
 import { CoreScenarioComponent } from "./core-scenario.component";
+import { ScenariosListComponent } from "../scenarios-list/scenarios-list.component";
+import { ScenarioComponent } from './scenario/scenario.component';
+
+import {ScenariosService} from "../service/scenarios.service";
+import {TaskService} from '../service/tasks.service';
 
 @NgModule({
 
   imports: [
 
     CommonModule,
-    CoreScenarioRoutingModule
+    CoreScenarioRoutingModule,
+    MdButtonModule,
+    MdMenuModule
 
   ],
-  declarations: [ CoreScenarioComponent ]
+  declarations: [
+
+    CoreScenarioComponent,
+    ScenariosListComponent,
+    ScenarioComponent
+
+  ],
+  providers: [
+
+    ScenariosService,
+    TaskService
+
+  ]
 
 })export class CoreScenarioModule { }
