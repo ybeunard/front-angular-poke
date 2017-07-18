686f564aa22eb242203f40036ce22d37f34bed2d
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-07-18 13:27:02 +0200

    [FEATURE] - Update envionment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,28 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-const urlServeurBack: string = "http://172.18.36.38:5000";
+const urlServeurBack: string = "http://172.18.36.112:5000";
 
 export const environment: any = {
 
   production: false,
+
+  keyCacheInstanceRunning: "instanceRunning",
+  intervalRequeteLog: 5000,
+
   urlGetAllActions: urlServeurBack + "/modules/*/actions/",
-  urlPostAction: urlServeurBack + "/modules/",
-  urlGetAllModules: urlServeurBack + "/modules/"
+  urlGetAction: urlServeurBack + "/modules/*/actions/id",
+  urlExecuteAction: urlServeurBack + "/modules/*/actions/id/execute",
+
+  urlGetAllModules: urlServeurBack + "/modules/",
+
+  urlGetAllScenario: urlServeurBack + "/scenarios/?lazy",
+  urlGetScenario: urlServeurBack + "/scenarios/id",
+  urlPutScenario: urlServeurBack + "/scenarios/",
+  urlPostScenario: urlServeurBack + "/scenarios/id",
+
+  urlGetAllInstancesStatusWorking: urlServeurBack + "/scenarios/instances/?status=WORKING&lazy",
+  urlGetInstance: urlServeurBack + "/scenarios/instances/id",
+  urlExecuteInstanceScenario: urlServeurBack + "/scenarios/id/execute"
 
 };
