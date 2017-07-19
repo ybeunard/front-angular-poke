55ca2583025512b199e99bf392d805fb6285c71b
Yoann Beunardeau <ext.yoann.beunardeau@lotsys.com>
2017-07-19 15:19:58 +0200

    [REFACTO] - Clean modules service

--- src/app/service/modules.service.mock.ts
+++ src/app/service/modules.service.mock.ts
@@ -1,8 +1,1 @@
-export const mockGetAllModules: Array<any> = [
-    { id: 0, label: "module 0", actions: [] },
-    { id: 1, label: "module 1", actions: [] },
-    { id: 2, label: "module 2", actions: [] },
-    { id: 3, label: "module 3", actions: [] },
-];
-
-export class ModulesServiceMock { };
+export class ModulesServiceMock { }
--- src/app/service/modules.service.ts
+++ src/app/service/modules.service.ts
@@ -17,7 +17,7 @@ export class ModulesService {
   private listModules: Array<Module> = [];
 
   // return observable list of all modules with a visibility set at false
-  getAllModules(): Observable<Array<Module>> {
+  public getAllModules(): Observable<Array<Module>> {
 
     if(this.listModules && this.listModules.length > 0) {
 
@@ -27,6 +27,11 @@ export class ModulesService {
     return this.http.get(environment.urlGetAllModules)
       .map(response => {
 
+        if(response.status === 204) {
+
+          return [];
+
+        }
         const responseModules: Array<any> = response.json().modules || { };
         responseModules.forEach((module: any) => {
 
@@ -36,19 +41,12 @@ export class ModulesService {
         return this.listModules;
 
       })
-      .catch(error => ModulesService.handleError(error , "getAllModules: "));
-
-  }
-
-  // refresh listModules after call update on Modules and Actions table on database
-  refreshAllModules() {
-
-    this.listModules = [];
+      .catch(error => ModulesService.handleError(error));
 
   }
 
   // return observable label of one module id in params
-  getLabelModule(idModule: number): Observable<string> {
+  public getLabelModule(idModule: number): Observable<string> {
 
     return this.getAllModules()
       .map(response => {
@@ -65,46 +63,26 @@ export class ModulesService {
 
   }
 
-  //return observable of all moduleId of all task in params
-  foundModuleId(tasks: Array<Task>): Observable<Array<{idModule: number, idTask: number}>> {
+  // refresh listModules after call update on Modules and Actions table on database
+  private refreshAllModules() {
 
-    return this.getAllModules()
-      .map(response => {
-
-          let moduleId: Array<{idModule: number, idTask: number}> = [];
-          response.forEach(function (module: Module) {
-
-            tasks.forEach(function (task: Task) {
-
-              if(module.actions.findIndex((action: Action) => action.id === task.inner_scenario_id) > -1) {
-
-                moduleId.push({idModule: module.id, idTask: task.inner_scenario_id});
-
-              }
-
-            });
-
-          });
-          return moduleId;
-
-      });
+    this.listModules = [];
 
   }
 
   constructor(private http: Http) { }
 
-  private static handleError (error: Response | any, functionName: string) {
+  private static handleError (error: Response | any) {
 
-    let errMsg: string = "ModulesService: " + functionName;
+    let errMsg: string;
     if (error instanceof Response) {
 
       const body: any = error.json() || "";
-      const err: string = body.error || JSON.stringify(body);
-      errMsg += `${error.status} - ${error.statusText || ""} ${err}`;
+      errMsg = `${error.status} - ${body.message || ""}`;
 
     } else {
 
-      errMsg += error.message ? error.message : error.toString();
+      errMsg = error.message ? error.message : error.toString();
 
     }
     return Observable.throw(errMsg);
